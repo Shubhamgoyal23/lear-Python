4dbc42b72a75b93c37c4e388dd97b071b4a05c4c
Shubham goyal <shubhamkuma2310@gmail.com>
2026-05-27 18:34:39 +0530

    Add files via upload

---
+++ assi_2.py
@@ -0,0 +1,70 @@
+# guess the output qus
+#1. What is the output of the following code? 
+a = 5 
+b = 10 
+print(a < b < 20) # true 
+
+
+#2. Predict the output: 
+x = True 
+y = False 
+print(x + y * x) #1
+
+#3. What does this expression evaluate to? 
+print(4 ** 0 ** 2)#1
+
+#4 guess the output
+print((3 and 0) or (0 and 3)) #0
+
+#5 find the result ofthe bitwise opretion
+a = 12
+b = 5
+print(a^b) # d
+
+# 6 what teh output of this tricky
+#print(256 is 256)
+#print(257 is 257)
+
+
+#7 evaluate this expessuon
+a = 7
+print(~a+1) #d
+
+
+# 8 what will this print
+a = True
+b = False
+print((a or b) and not(a and b))
+
+
+''' a or b = true
+    a and b = false
+    not (a and b)= true
+    (a or b) and (not(a and b))= true
+    '''
+
+#9  what is the ouput
+print(10>5 ==True)
+
+
+#10. Evaluate this expression: 
+print(2 + 3 * 4 ** 2 // 8 % 3) 
+
+
+#12. Predict the output: 
+a = 3 
+b = 2 
+a *= b + 1 
+print(a) 
+
+
+#13 evalute this comparison
+
+print(3<5>2==2)
+
+
+#14 what is the result
+
+x = 10
+y =20
+print(x&y|x^y)
